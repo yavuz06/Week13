f751453f690af029e9c7348921e3d49b1d6f4eb0
yavuz06 <yavuzselmkaya@gmail.com>
2023-05-17 15:55:27 +0300

    week13

---
+++ Week13SQLLdb/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Data.SQLite;
+
+ReadData(CreateConnection());
+
+AddCustomer(CreateConnection());
+
+static SQLiteConnection CreateConnection()
+{
+
+
+    SQLiteConnection connection = new SQLiteConnection("Data Source=mydb.db; Version=3; New=True Compress=True");
+
+    try
+    {
+        connection.Open();
+        Console.WriteLine("Connectıon established");
+
+    }
+    catch
+    {
+        Console.WriteLine("DB connection failed");
+    }
+    return connection;
+
+}
+
+static void ReadData(SQLiteConnection myConnection)
+{
+
+    SQLiteDataReader read;
+    SQLiteCommand command;
+
+    command = myConnection.CreateCommand();
+    command.CommandText = "Select * From Customer";
+
+    read = command.ExecuteReader();
+
+    while (read.Read())
+    {
+        string fName = read.GetString(0);
+        string lName = read.GetString(1);
+        string dob = read.GetString(2);
+        Console.WriteLine($"Full name : {fName} {lName}; DoB {dob}");
+
+    }
+
+
+    myConnection.Close();
+}
+
+static void AddCustomer(SQLiteConnection myConnection)
+{
+    SQLiteCommand command;
+
+    string fName = "Harry";
+    string lName = "Potter";
+    string dob = "07-31-1980";
+
+    command = myConnection.CreateCommand();
+    command.CommandText = $"INSERT INTO customer(firstName,lastName,dateOfBirth) VALUES ('{fName}', '{lName} ', '{dob}')";
+
+    int rowInserted = command.ExecuteNonQuery();
+
+    Console.Clear();
+    Console.WriteLine($"Rows inserted: {rowInserted}");
+
+    ReadData(myConnection);
+
+    myConnection.Close();
+
+}
+
